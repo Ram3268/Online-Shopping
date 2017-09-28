a0347e986f5595130108d5d88cf4663cdaa33745
ram <Ram Gangineni@192.168.0.26>
2017-09-27 21:28:45 -0500

    Succesfully Loaded

--- OnlineShopping/src/main/java/com/v4s/spring/OnlineShopping/PageController.java
+++ OnlineShopping/src/main/java/com/v4s/spring/OnlineShopping/PageController.java
@@ -9,13 +9,39 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 	
-@RequestMapping(value= {"/","/home","/index"})
+@RequestMapping(value= {"/home"})
 public ModelAndView index() {
 	
 	ModelAndView mv = new ModelAndView("page");
-	mv.addObject("greeting","Welcome ");
+	mv.addObject("title","Home");
+	mv.addObject("userClickHome",true);
 	return mv;
 }
 
+@RequestMapping(value= {"/about"})
+public ModelAndView about() {
+	
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title","About");
+	mv.addObject("userClickAbout",true);
+	return mv;
+}
 
+@RequestMapping(value= {"/contact"})
+public ModelAndView contact() {
+	
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title","Contact");
+	mv.addObject("userClickContact",true);
+	return mv;
+}
+
+@RequestMapping(value= {"/products"})
+public ModelAndView products() {
+	
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title","Products");
+	mv.addObject("userClickProducts",true);
+	return mv;
+}
 }
