186eaa95e824111396cb5107c77024f3104385dc
ram <Ram Gangineni@192.168.0.43>
2017-10-03 13:04:35 -0500

    Product Testing success

---
+++ shoppingbackend/src/main/java/com/v4s/shoppingbackend/dao/ProductDAO.java
@@ -0,0 +1,23 @@
+package com.v4s.shoppingbackend.dao;
+
+import java.util.List;
+
+import com.v4s.shoppingbackend.dto.Product;
+
+public interface ProductDAO {
+	
+	Product get(int productId);
+	List<Product> list();	
+	boolean add(Product product);
+	boolean update(Product product);
+	boolean delete(Product product);
+
+	List<Product> getProductsByParam(String param, int count);	
+	
+	
+	// business methods
+	List<Product> listActiveProducts();	
+	List<Product> listActiveProductsByCategory(int categoryId);
+	List<Product> getLatestActiveProducts(int count);
+
+}
